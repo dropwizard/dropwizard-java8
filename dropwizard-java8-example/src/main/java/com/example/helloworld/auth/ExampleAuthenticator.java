f9bcede0e710837a65bb07db4bcdc152ae74a47b
Jochen Schalanda <jochen@schalanda.name>
2014-07-18 14:22:55 +0200

    Add dropwizard-java8-example

---
+++ dropwizard-java8-example/src/main/java/com/example/helloworld/auth/ExampleAuthenticator.java
@@ -0,0 +1,18 @@
+package com.example.helloworld.auth;
+
+import com.example.helloworld.core.User;
+import com.github.joschi.dropwizard.java8.auth.Authenticator;
+import io.dropwizard.auth.AuthenticationException;
+import io.dropwizard.auth.basic.BasicCredentials;
+
+import java.util.Optional;
+
+public class ExampleAuthenticator implements Authenticator<BasicCredentials, User> {
+    @Override
+    public Optional<User> authenticate(BasicCredentials credentials) throws AuthenticationException {
+        if ("secret".equals(credentials.getPassword())) {
+            return Optional.of(new User(credentials.getUsername()));
+        }
+        return Optional.empty();
+    }
+}
---
+++ dropwizard-java8-example/src/main/java/com/example/helloworld/health/TemplateHealthCheck.java
@@ -0,0 +1,21 @@
+package com.example.helloworld.health;
+
+import com.codahale.metrics.health.HealthCheck;
+import com.example.helloworld.core.Template;
+
+import java.util.Optional;
+
+public class TemplateHealthCheck extends HealthCheck {
+    private final Template template;
+
+    public TemplateHealthCheck(Template template) {
+        this.template = template;
+    }
+
+    @Override
+    protected Result check() throws Exception {
+        template.render(Optional.of("woo"));
+        template.render(Optional.<String>empty());
+        return Result.healthy();
+    }
+}
