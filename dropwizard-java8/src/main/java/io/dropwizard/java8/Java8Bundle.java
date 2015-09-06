e5ab641b6c6cc8e98b6eef6616eef89f5fbd8228
Justin Rudd <justin.rudd@gmail.com>
2015-09-06 15:16:58 -0400

    Fixes Java8Bundle to use DW classes and correct Jackson module classes.
    * Replaces deprecated JSR310Module with JavaTimeModule
    * Uses DropWizard Validators class instead of using java.validation.Validation so that it will pick up the
      standard DropWizard validation value handlers

--- dropwizard-java8/src/main/java/io/dropwizard/java8/Java8Bundle.java
+++ dropwizard-java8/src/main/java/io/dropwizard/java8/Java8Bundle.java
@@ -1,30 +1,27 @@
 package io.dropwizard.java8;
 
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
-import com.fasterxml.jackson.datatype.jsr310.JSR310Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import io.dropwizard.Bundle;
 import io.dropwizard.java8.jersey.OptionalMessageBodyWriter;
 import io.dropwizard.java8.jersey.OptionalParamFeature;
 import io.dropwizard.java8.validation.valuehandling.OptionalValidatedValueUnwrapper;
+import io.dropwizard.jersey.validation.Validators;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
-import org.hibernate.validator.HibernateValidator;
 
-import javax.validation.Validation;
 import javax.validation.ValidatorFactory;
 
 public class Java8Bundle implements Bundle {
+
     @Override
     public void initialize(final Bootstrap<?> bootstrap) {
-        bootstrap.getObjectMapper().registerModules(new Jdk8Module());
-        bootstrap.getObjectMapper().registerModules(new JSR310Module());
+        bootstrap.getObjectMapper().registerModules(new Jdk8Module(), new JavaTimeModule());
 
-        final ValidatorFactory validatorFactory = Validation
-                .byProvider(HibernateValidator.class)
-                .configure()
-                .addValidatedValueHandler(new io.dropwizard.validation.valuehandling.OptionalValidatedValueUnwrapper())
-                .addValidatedValueHandler(new OptionalValidatedValueUnwrapper())
-                .buildValidatorFactory();
+        final ValidatorFactory validatorFactory =
+                Validators.newConfiguration()
+                    .addValidatedValueHandler(new OptionalValidatedValueUnwrapper())
+                    .buildValidatorFactory();
         bootstrap.setValidatorFactory(validatorFactory);
     }
 
@@ -33,4 +30,5 @@ public class Java8Bundle implements Bundle {
         environment.jersey().register(OptionalMessageBodyWriter.class);
         environment.jersey().register(OptionalParamFeature.class);
     }
+
 }
