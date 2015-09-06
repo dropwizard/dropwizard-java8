971ca02e61cc745f415f15766644fcd731aab5c6
Justin Rudd <justin.rudd@gmail.com>
2015-09-06 15:39:53 -0400

    Updates Instant argument and mapper to use a TimeZone
    Passes the databaseTimeZone from the DBIFactory to InstantArgumentFactory and InstantMapper.

--- dropwizard-java8-jdbi/src/main/java/io/dropwizard/java8/jdbi/DBIFactory.java
+++ dropwizard-java8-jdbi/src/main/java/io/dropwizard/java8/jdbi/DBIFactory.java
@@ -12,6 +12,8 @@ import io.dropwizard.java8.jdbi.args.OptionalArgumentFactory;
 import io.dropwizard.setup.Environment;
 import org.skife.jdbi.v2.DBI;
 
+import java.util.TimeZone;
+
 public class DBIFactory extends io.dropwizard.jdbi.DBIFactory {
     @Override
     public DBI build(Environment environment, PooledDataSourceFactory configuration, ManagedDataSource dataSource, String name) {
@@ -19,12 +21,24 @@ public class DBIFactory extends io.dropwizard.jdbi.DBIFactory {
 
         dbi.registerArgumentFactory(new OptionalArgumentFactory(configuration.getDriverClass()));
         dbi.registerContainerFactory(new OptionalContainerFactory());
+
         dbi.registerArgumentFactory(new LocalDateArgumentFactory());
         dbi.registerArgumentFactory(new LocalDateTimeArgumentFactory());
-        dbi.registerArgumentFactory(new InstantArgumentFactory());
-        dbi.registerMapper(new InstantMapper());
         dbi.registerMapper(new LocalDateMapper());
         dbi.registerMapper(new LocalDateTimeMapper());
+
+        final com.google.common.base.Optional<TimeZone> gtz = this.databaseTimeZone();
+
+        final java.util.Optional<TimeZone> tz;
+        if (gtz.isPresent()) {
+            tz = java.util.Optional.of(gtz.get());
+        }
+        else {
+            tz = java.util.Optional.empty();
+        }
+
+        dbi.registerArgumentFactory(new InstantArgumentFactory(tz));
+        dbi.registerMapper(new InstantMapper(tz));
 
         return dbi;
     }
--- dropwizard-java8-jdbi/src/main/java/io/dropwizard/java8/jdbi/args/InstantArgumentFactory.java
+++ dropwizard-java8-jdbi/src/main/java/io/dropwizard/java8/jdbi/args/InstantArgumentFactory.java
@@ -5,11 +5,32 @@ import org.skife.jdbi.v2.tweak.Argument;
 import org.skife.jdbi.v2.tweak.ArgumentFactory;
 
 import java.time.Instant;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Optional;
+import java.util.TimeZone;
 
 /**
  * An {@link ArgumentFactory} for {@link Instant} arguments.
  */
 public class InstantArgumentFactory implements ArgumentFactory<Instant> {
+
+    /**
+     * <p>{@link Calendar} for representing a database time zone.<p>
+     * If a field is not represented in a database as
+     * {@code TIMESTAMP WITH TIME ZONE}, we need to set its time zone
+     * explicitly. Otherwise it will not be correctly represented in
+     * a time zone different from the time zone of the database.
+     */
+    private final Optional<Calendar> calendar;
+
+    public InstantArgumentFactory() {
+        this(Optional.empty());
+    }
+
+    public InstantArgumentFactory(final Optional<TimeZone> tz) {
+        this.calendar = tz.map(GregorianCalendar::new);
+    }
 
     @Override
     public boolean accepts(Class<?> expectedType, Object value, StatementContext ctx) {
@@ -18,6 +39,6 @@ public class InstantArgumentFactory implements ArgumentFactory<Instant> {
 
     @Override
     public Argument build(Class<?> expectedType, Instant value, StatementContext ctx) {
-        return new InstantArgument(value);
+        return new InstantArgument(value, calendar);
     }
 }
